b0387107fff66c329e10086e9dcb22e2f86ec903
ArnoStrouwen <arno.strouwen@telenet.be>
2026-03-13 11:01:54 +0100

    bump ODE_BUDGET_TRAJ to 6.25M (profiler-confirmed on H100)
    Profiler with ga=2 on H100 found C_max=6.25M. This gives L=290, M=292
    (35% increase over original L=214, M=216 at C=5M with ga=1).

--- examples/monod/model.jl
+++ examples/monod/model.jl
@@ -70,7 +70,7 @@ const N_NOISE_CHANNELS = 1  # single additive Gaussian noise
 
 include(joinpath(@__DIR__, "..", "..", "src", "utils.jl"))
 
-const ODE_BUDGET_TRAJ = 5_000_000
+const ODE_BUDGET_TRAJ = 6_250_000
 const GRAD_ACCUM_STEPS = 2
 const (L_CONTRASTIVE, M_NUISANCE, _B_MICRO) = allocate_budget(ODE_BUDGET_TRAJ; B_multiplier=GRAD_ACCUM_STEPS)
 const GRAD_BATCH = _B_MICRO * GRAD_ACCUM_STEPS
